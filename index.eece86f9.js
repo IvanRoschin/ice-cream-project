7e0b9be51d66b7c60cc0cc630e4e1b36081576a6
alvasar <103255514+alvasar@users.noreply.github.com>
2022-06-26 15:45:49 +0000

    Deploying to gh-pages from @ alvasar/ice-cream-projet@efe4df9957028726ff0697165209e93399d5df70 🚀

---
+++ index.eece86f9.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),s=document.querySelector("[is-hiden]");e.addEventListener("click",(()=>{const n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),s.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.eece86f9.js.map
