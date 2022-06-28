cff8594ea8eea7710cee66085adb06ca259300ed
alvasar <103255514+alvasar@users.noreply.github.com>
2022-06-28 17:38:23 +0000

    Deploying to gh-pages from @ alvasar/ice-cream-project@15481d09994fe26020ba1e6917a886dd0a815fd0 🚀

---
+++ index.32a50408.js
@@ -0,0 +1,2 @@
+(()=>{const t={openModalBtn:document.querySelector("[data-product-open]"),closeModalBtn:document.querySelector("[data-product-close]"),modalcontact:document.querySelector("[data-product]")};function e(){t.modalcontact.classList.toggle("is-hidden")}t.openModalBtn.addEventListener("click",e),t.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.32a50408.js.map
