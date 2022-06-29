79407bbc20783e08af08b37a064d57b38cb95fe2
alvasar <103255514+alvasar@users.noreply.github.com>
2022-06-29 05:09:42 +0000

    Deploying to gh-pages from @ alvasar/ice-cream-project@d81e71751aa50b7d9c2f73e3d4e0eb05123050b1 🚀

--- index.23141aaa.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-header-open]"),closeModalBtn:document.querySelector("[data-modal-header-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.23141aaa.js.map
